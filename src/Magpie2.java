9db015e35e1291b1e827603ecee12d7b3cbcaddd
Luciano <jluce1996@gmail.com>
2014-10-03 10:06:49 -0400

    beginning to change code

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -39,10 +39,29 @@ public class Magpie2
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0)
+
 		{
 			response = "Tell me more about your family.";
 		}
-		else
+        else if (statement.indexOf("cat") >= 0
+                || statement.indexOf("dog") >= 0)
+        {
+            response = "Tell me more about your pets.";
+        }
+        else if (statement.indexOf("Mrs.") >= 0
+                || statement.indexOf("Miss") >= 0)
+        {
+            response = "She sounds like a good teacher.";
+        }
+        else if (statement.indexOf("Mr.") >= 0)
+        {
+            response = "He sounds like a good teacher.";
+        }
+        else if (statement.indexOf("") >= 0)
+        {
+            response = "Say something, please.";
+        }
+        else
 		{
 			response = getRandomResponse();
 		}
